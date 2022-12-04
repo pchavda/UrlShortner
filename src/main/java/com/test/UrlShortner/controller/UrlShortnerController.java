c9b2bb3f8748a3f64a1a5a3e67f4c97e0a0d4b82
Ishan Shah <ishanshah@Ishans-MBP.hsd1.ca.comcast.net>
2022-12-04 11:00:33 -0800

    Adding logic to service class

--- src/main/java/com/test/UrlShortner/controller/UrlShortnerController.java
+++ src/main/java/com/test/UrlShortner/controller/UrlShortnerController.java
@@ -1,20 +1,36 @@
 package com.test.UrlShortner.controller;
 
+import com.test.UrlShortner.model.ErrorResponse;
+import com.test.UrlShortner.model.Response;
+import com.test.UrlShortner.service.UrlShortenerService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 @Slf4j
 public class UrlShortnerController {
-
+    @Autowired
+    UrlShortenerService urlShortenerService;
     @GetMapping("/url")
-    public String getUrl(@RequestParam String shortendUrl){
-        log.info("Request URL:{}",shortendUrl);
-        return "The shortend url passed :"+shortendUrl;
+    public String getUrl(@RequestParam String shortenedUrl) {
+        log.info("Request URL:{}", shortenedUrl);
+        return "The shortened url passed :" + shortenedUrl;
     }
-    @PostMapping("/url/")
-    public String createUrl(@RequestBody String url){
-        return "Generate url shortner";
+
+    @PostMapping("/shortenURL/")
+    public Object createUrl(@RequestBody String url) {
+        if(urlShortenerService.validateURL(url)){
+            String shortenedURL=urlShortenerService.shortenURL(url);
+            return new Response(url,shortenedURL);
+        }
+        else{
+            ErrorResponse errorResponse = urlShortenerService.buildErrorResponseObject(url);
+            return errorResponse;
+        }
+       // return "Generate URL shortener";
     }
+
+
 }
--- src/main/java/com/test/UrlShortner/service/UrlShortenerService.java
+++ src/main/java/com/test/UrlShortner/service/UrlShortenerService.java
@@ -1,7 +1,62 @@
 package com.test.UrlShortner.service;
 
+import com.test.UrlShortner.model.ErrorResponse;
+import org.apache.commons.validator.routines.UrlValidator;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UrlShortenerService {
+
+     public boolean validateURL(String url) {
+
+         String[] schemes = {"http","https"};
+         UrlValidator urlValidator = new UrlValidator(schemes);
+         if (urlValidator.isValid(url)) {
+             System.out.println("url is valid");
+             return true;
+         } else {
+             return false;
+         }
+    }
+
+    public String shortenURL(String url) {
+          String newURL=getAlphaNumericString(10);
+         //check if String present in H2
+            return newURL;
+
+    }
+
+    public ErrorResponse buildErrorResponseObject(String url) {
+        ErrorResponse errorResponse=new ErrorResponse();
+        errorResponse.setErrorMessage("Invalid URL" + url);
+        return errorResponse;
+    }
+
+    static String getAlphaNumericString(int n)
+    {
+
+        // chose a Character random from this String
+        String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                + "0123456789"
+                + "abcdefghijklmnopqrstuvxyz";
+
+        // create StringBuffer size of AlphaNumericString
+        StringBuilder sb = new StringBuilder(n);
+
+        for (int i = 0; i < n; i++) {
+
+            // generate a random number between
+            // 0 to AlphaNumericString variable length
+            int index
+                    = (int)(AlphaNumericString.length()
+                    * Math.random());
+
+            // add Character one by one in end of sb
+            sb.append(AlphaNumericString
+                    .charAt(index));
+        }
+
+        return sb.toString();
+    }
 }
+
